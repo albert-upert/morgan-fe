d8f8ba855fdb7e33283f707dad1820f16b6ccc0d
raihanpakbar <raihanp.akbar@gmail.com>
2026-02-07 20:08:48 +0700

    SCRUM-16 [FE_Slicing_FM/IT_Ticket List_Accept & Recieve Ticket from Dosen] Ticket List Page

--- src/routes/_layout/fm-it/ticket-list.tsx
+++ src/routes/_layout/fm-it/ticket-list.tsx
@@ -1,6 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getUser } from "@/lib/auth";
 import { TicketListView } from "@/views/fm-it/TicketListPageView";
 
 export const Route = createFileRoute("/_layout/fm-it/ticket-list")({
+  beforeLoad: async ({ location, context }) => {
+    const user = await getUser(context.queryClient);
+    if (!user) {
+      throw redirect({
+        to: "/login",
+        search: {
+          redirect: location.href,
+        },
+      });
+    }
+  },
   component: TicketListView,
 });
