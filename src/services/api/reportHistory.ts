490f060121a1e0bd1348e3ee6be2ec6d99ec263f
raihanpakbar <raihanp.akbar@gmail.com>
2026-02-10 10:00:32 +0700

    [IT] [AddFeatures] Add mock API for reporting histories

--- src/services/api/reportHistory.ts
+++ src/services/api/reportHistory.ts
@@ -1,6 +1,5 @@
 import { MOCK_TIMELINES } from "@/services/mock/mockHistories.ts";
-import { reportsSeed } from "@/services/mock/mockReport.ts";
-import type { TicketHistory, TicketStatus } from "@/types/ticketHistory";
+import type { TicketHistory } from "@/types/ticketHistory";
 
 const delay = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
@@ -21,30 +20,4 @@ export const getTimelineByTicketId = async (
   });
 
   return histories;
-};
-
-export const updateTicketStatus = async (
-  reportId: string,
-  newStatus: TicketStatus,
-  note?: string
-): Promise<TicketHistory> => {
-  await new Promise((r) => setTimeout(r, 500));
-
-  const newHistory: TicketHistory = {
-    id: `H-${Date.now()}`,
-    reportId: reportId,
-    status: newStatus,
-    technicalNotes: note || "",
-    timestamp: new Date().toISOString(),
-    actor: "Budi Santoso",
-  };
-
-  MOCK_TIMELINES.push(newHistory);
-
-  const reportIndex = reportsSeed.findIndex((r) => r.id === reportId);
-  if (reportIndex !== -1) {
-    reportsSeed[reportIndex].status = newStatus;
-  }
-
-  return newHistory;
 };
