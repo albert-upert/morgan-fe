06bf801dd4bb15c2a8f66ce6f5809991273453db
raihanpakbar <raihanp.akbar@gmail.com>
2026-02-10 12:03:59 +0700

    SCRUM-18 [FE_Slicing_FM/IT_Ticket List_Travel & Resolve Issue] Ticket Detail Page-Detail Tiket

--- src/routes/_layout/fm-it/ticket-detail.$id.tsx
+++ src/routes/_layout/fm-it/ticket-detail.$id.tsx
@@ -4,3 +4,22 @@ import { TicketDetailView } from "@/views/fm-it/TicketDetailPageView";
 export const Route = createFileRoute("/_layout/fm-it/ticket-detail/$id")({
   component: TicketDetailView,
 });
+
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getUser } from "@/lib/auth";
+import { TicketDetailView } from "@/views/fm-it/TicketDetailPageView";
+
+export const Route = createFileRoute("/_layout/fm-it/ticket-detail/$id")({
+  beforeLoad: async ({ location, context }) => {
+    const user = await getUser(context.queryClient);
+    if (!user) {
+      throw redirect({
+        to: "/login",
+        search: {
+          redirect: location.href,
+        },
+      });
+    }
+  },
+  component: TicketDetailView,
+});
