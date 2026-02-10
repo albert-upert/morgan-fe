0e8648c532a5b630d0aac2ff4849d9b68dc83e8a
raihanpakbar <raihanp.akbar@gmail.com>
2026-02-10 10:00:32 +0700

    [IT] [AddFeatures] Add mock API for reporting histories

---
+++ src/services/mock/mockHistories.ts
@@ -0,0 +1,18 @@
+import type { TicketHistory } from "@/types/ticketHistory.ts";
+
+export const MOCK_TIMELINES: Array<TicketHistory> = [
+  {
+    id: "H-1",
+    reportId: "#FM-2025-0103",
+    status: "Menunggu Diterima oleh Petugas",
+    timestamp: "2025-10-05T08:09:00",
+    actor: "Budi Santoso",
+  },
+  {
+    id: "H-2",
+    reportId: "#FM-2025-0103",
+    status: "Petugas dalam Perjalanan",
+    timestamp: "2025-10-05T08:11:00",
+    actor: "Budi Santoso",
+  },
+];
