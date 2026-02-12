56c0fb839433d71dc8c6d268e86c246aab5bf7d2
raihanpakbar <raihanp.akbar@gmail.com>
2026-02-13 06:12:56 +0700

    [Remove] Removing BeforeLoad so that all routes only rendering views components

--- src/routes/_layout/fm-it/home.tsx
+++ src/routes/_layout/fm-it/home.tsx
@@ -1,18 +1,6 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import { getUser } from "@/lib/auth";
+import { createFileRoute } from "@tanstack/react-router";
 import { FmitHomePageView } from "@/views/fm-it/FmitHomePageView";
 
 export const Route = createFileRoute("/_layout/fm-it/home")({
-  beforeLoad: async ({ location, context }) => {
-    const user = await getUser(context.queryClient);
-    if (!user) {
-      throw redirect({
-        to: "/login",
-        search: {
-          redirect: location.href,
-        },
-      });
-    }
-  },
   component: FmitHomePageView,
 });
--- src/routes/_layout/fm-it/ticket-detail.$id.tsx
+++ src/routes/_layout/fm-it/ticket-detail.$id.tsx
@@ -1,18 +1,6 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import { getUser } from "@/lib/auth";
+import { createFileRoute } from "@tanstack/react-router";
 import { TicketDetailView } from "@/views/fm-it/TicketDetailPageView";
 
 export const Route = createFileRoute("/_layout/fm-it/ticket-detail/$id")({
-  beforeLoad: async ({ location, context }) => {
-    const user = await getUser(context.queryClient);
-    if (!user) {
-      throw redirect({
-        to: "/login",
-        search: {
-          redirect: location.href,
-        },
-      });
-    }
-  },
   component: TicketDetailView,
 });
