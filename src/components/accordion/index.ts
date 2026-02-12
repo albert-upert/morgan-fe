60df8fa7706984c612b482f3d1d43508afe55be4
raihanpakbar <raihanp.akbar@gmail.com>
2026-02-12 18:51:10 +0700

    [Remove] Removing components that are available in uper components

--- src/components/accordion/index.ts
+++
@@ -1,8 +0,0 @@
-export { Accordion, AccordionGroup, AccordionRow } from "./accordion";
-export type {
-  AccordionProps,
-  AccordionGroupProps,
-  AccordionRowProps,
-  AccordionVariant,
-  AccordionDataItem,
-} from "./accordion";
